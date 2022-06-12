c3a5702c939e65430f246451ae6e5c465d72a39b
sahoooii <saho.chu@gmail.com>
2022-06-12 17:22:33 +0900

    created a swipes function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Swipe;
 
 class UserController extends Controller
 {
     public function index()
     {
         // $user = User::find(1);
-        //auth userではないuserを取得
-        $user = User::where('id', '<>', \Auth::user()->id)->first();
+
+        //すでにswipeしたuserを省いて、idsを取得
+        $swipedUserIds = Swipe::where('from_user_id', \Auth::user()->id )->get()->pluck('to_user_id');
+        //swipeしていないuserを1つ取得
+        $user = User::where('id', '<>', \Auth::user()->id)->whereNotIn('id', $swipedUserIds)->first();
 
         return view('pages.user.index', [
             'user' => $user
