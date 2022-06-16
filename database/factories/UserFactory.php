4d8ad2d70b6954146699d2b7183f32db77cb939d
sahoooii <saho.chu@gmail.com>
2022-06-16 15:55:24 +0900

    add an img dummy data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,6 +12,7 @@ class UserFactory extends Factory
      *
      * @return array
      */
+
     public function definition()
     {
         return [
@@ -19,6 +20,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'img_url' => $this->faker->imageUrl(),
             'remember_token' => Str::random(10),
         ];
     }
