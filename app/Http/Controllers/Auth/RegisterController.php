8cf4feb64fe19484bd771e75dafebca292079655
sahoooii <saho.chu@gmail.com>
2022-06-21 17:38:23 +0900

    password confoirmationまで

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -55,7 +55,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:50'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -90,7 +90,7 @@ class RegisterController extends Controller
      * @param .. Request $request
      * @return \App\Models\User
      */
-    protected function create(Request $request)
+    static function create(Request $request)
     {
         //img upload
         // dd($request->all());
