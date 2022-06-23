0a435a37387ff2cbf34f1f09a57cc5af75229517
sahoooii <saho.chu@gmail.com>
2022-06-23 17:41:46 +0900

    ada a register view form

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -41,7 +41,7 @@ return [
     'different' => 'The :attribute and :other must be different.',
     'digits' => 'The :attribute must be :digits digits.',
     'digits_between' => 'The :attribute must be between :min and :max digits.',
-    'dimensions' => 'The :attribute has invalid image dimensions.',
+    'dimensions' => 'The :attribute has invalid, please input your correct :attribute.',
     'distinct' => 'The :attribute field has a duplicate value.',
     'email' => 'The :attribute must be a valid email address.',
     'ends_with' => 'The :attribute must end with one of the following: :values.',
