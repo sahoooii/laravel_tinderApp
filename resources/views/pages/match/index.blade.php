54a405837eba21668823fc84e565889c75d1d3b0
sahoooii <saho.chu@gmail.com>
2022-06-29 16:10:26 +0900

    Add a back button on matched.show, changed a design on mtched.index

--- resources/views/pages/match/index.blade.php
+++ resources/views/pages/match/index.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="p-match-index">
+{{-- <div class="p-match-index"> --}}
 	<div class="container">
-		<div class="col-md-12 mb-3 mt-2">
-			<p class="fs-5">Your Match</p>
-		</div>
-		<div class="row">
-			@foreach ($matchedUsers as $matchedUser)
-			{{-- @php
-					dd($matchedUser->toUser);
-			@endphp --}}
-				<div class="col-md-12 mb-3">
-					<img src="{{ $matchedUser->toUser->img_url }}" alt="img" class="rounded-circle" style="height: 70px; width: 70px; object-fit:cover;">
-					<a href="{{route('matches.show', ['id' =>  $matchedUser->toUser->id]) }}" class="streched-link ml-3" style="font-size: 16px;">
-						{{ $matchedUser->toUser->name }}
-					</a>
+		<div class="row justify-content-center">
+			<div class="col-md-12">
+				<div class="card mb-3">
+						<div class="card-header">Your Match</div>
 				</div>
-			@endforeach
+
+				<div class="row">
+					@foreach ($matchedUsers as $matchedUser)
+					{{-- @php
+							dd($matchedUser->toUser);
+					@endphp --}}
+						<div class="col-md-12 mb-3 ml-2">
+							<img src="{{ $matchedUser->toUser->img_url }}" alt="img" class="rounded-circle" style="height: 70px; width: 70px; object-fit:cover;">
+							<a href="{{route('matches.show', ['id' =>  $matchedUser->toUser->id]) }}" class="streched-link ml-4 text-decoration-none" style="font-size: 18px;">
+								{{ $matchedUser->toUser->name }}
+							</a>
+						</div>
+					@endforeach
+				</div>
 		</div>
 	</div>
-</div>
+{{-- </div> --}}
 
 @endsection
