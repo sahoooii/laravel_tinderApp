736a80460c024cb8e048c9972814445775397691
sahoooii <saho.chu@gmail.com>
2022-08-04 17:23:03 +0900

    必要のないコードを削除

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -15,3 +15,20 @@ class ImageService
         return $fullFilePath;
     }
 }
+
+		//InterventionImage
+
+        // if (is_array($imageFile)) {
+        //     $file = $imageFile['image'];
+        // } else {
+        //     $file = $imageFile;
+        // }
+
+        // $fileName = uniqid(rand(). '_');
+        // $extension = $file->extension();
+        // $fileNameToStore = $fileName. '.' . $extension;
+        // $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode();
+
+        // Storage::put('public/' . $folderName . '/' .$fileNameToStore, $resizedImage);
+
+        // return $fileNameToStore;
