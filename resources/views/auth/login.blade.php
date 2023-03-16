090208e469e069feba5fb792af7b4cf8e131e0b1
sahoooii <saho.chu@gmail.com>
2023-03-16 18:07:40 +0900

    route changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,11 +40,11 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-12">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
+                                    <label class="form-check-label ml-2" for="remember">
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
