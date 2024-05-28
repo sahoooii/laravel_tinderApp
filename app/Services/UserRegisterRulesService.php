83cf8feaec678c24d535aef50c4c6adc4fe30450
sahoooii <saho.chu@gmail.com>
2024-05-28 18:41:09 +0900

    Refactoring css at auth screen

--- app/Services/UserRegisterRulesService.php
+++ app/Services/UserRegisterRulesService.php
@@ -10,8 +10,8 @@ class UserRegisterRulesService
             'image.mimes' => 'The image must be a file of type: jpeg,png, jpg.',
             'age.between' => 'You can join us from age 18 to 55.',
             'height.between' => 'Sorry, we have a height requirements 140cm to 220cm.',
-            'search_gender.required' => 'Please select what you want to date.',
-            'search_status.required' => 'Please select what you looking for.',
+            'search_gender.required' => 'Please select what do you want to date.',
+            'search_status.required' => 'Please select what do you looking for.',
             'occupation.max' => 'Please input 200 characters or less.',
             'message.max' => 'Please input 3,000 characters or less.',
         ];
@@ -19,18 +19,18 @@ class UserRegisterRulesService
 
     public static function rules()
     {
-		return [
-			'name' => ['required', 'string','min:4','max:50'],
-			'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-			'password' => ['required', 'string', 'min:8', 'confirmed'],
-			'image' => ['required','image', 'mimes:png,jpg,jpeg', 'max:2048'],
-			'age' => ['required','integer','between: 18,55'],
-			'height' =>['required','numeric','between: 140,220'],
-			'gender' => ['required', 'boolean'],
-			'search_gender' => ['required'],
-			'search_status' => ['required'],
-			'occupation' => ['nullable', 'string','max:200'],
-			'message' => ['nullable', 'max:3000']
-		];
+        return [
+            'name' => ['required', 'string','min:4','max:50'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'min:6', 'confirmed'],
+            'image' => ['required','image', 'mimes:png,jpg,jpeg', 'max:2048'],
+            'age' => ['required','integer','between: 18,55'],
+            'height' =>['required','numeric','between: 140,220'],
+            'gender' => ['required', 'boolean'],
+            'search_gender' => ['required'],
+            'search_status' => ['required'],
+            'occupation' => ['nullable', 'string','max:200'],
+            'message' => ['nullable', 'max:3000']
+        ];
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,7 @@
 @extends('layouts.app')
 @section('content')
-<style>
-    .tbg {
-        height: 1480px;
-    }
 
-    /* .card {
-        height: 850px;
-    } */
-</style>
-
-<div class="container">
+<div class="container pt-3 pb-5">
     <div class="row justify-content-center">
         <div class="col-12">
             <div class="card">
@@ -20,7 +11,7 @@
                     <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="row mb-3">
-                            <input type="file" name="image" accept="image/jpeg, image/jpg, image/png" class="col-8 col-form-label text-md-end mx-auto  @error('image') is-invalid @enderror"   style="font-size: 8px;">
+                            <input type="file" name="image" accept="image/jpeg, image/jpg, image/png" class="col-8 col-form-label text-md-end mx-auto  @error('image') is-invalid @enderror"  style="font-size: 8px;">
                             @error('image')
                                 <span class="invalid-feedback text-center" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -30,10 +21,8 @@
 
                         <div class="row mb-3">
                             <label for="name" class="col-form-label">{{ __('Name') }}</label>
-
                             <div class="col-12">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"   autocomplete="name" autofocus>
-
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -44,10 +33,8 @@
 
                         <div class="row mb-3">
                             <label for="email" class="col-form-label">{{ __('Email') }}</label>
-
                             <div class="col-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"   autocomplete="email">
-
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -58,10 +45,8 @@
 
                         <div class="row mb-3">
                             <label for="password" class="col-form-label">{{ __('Password') }}</label>
-
                             <div class="col-12">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}"   autocomplete="new-password">
-
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -72,84 +57,95 @@
 
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-form-label">{{ __('Confirm Password') }}</label>
-
                             <div class="col-12">
                                 <input id="password-confirm" type="password" class="form-control mt-2" name="password_confirmation" value="{{ old('password_confirmation') }}"   autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="d-flex justify-content-between">
-                            <div class="row mb-3">
-                                <label for="age" class="col-form-label fs-6">{{ __('Age') }}</label>
+                        <div class="mb-3">
+                          <div class="d-flex justify-content-between">
+                              <div class="row mr-1">
+                                  <label for="age" class="col-form-label fs-6">{{ __('Age') }}</label>
+                                  <div>
+                                      <input id="age" type="number" class="form-control text-center @error('age') is-invalid @enderror" name="age" value="{{ old('age') }}" autocomplete="age">
+                                  </div>
+                              </div>
 
-                                <div class="col-12">
-                                    <input id="age" type="number" class="form-control text-center @error('age') is-invalid @enderror" name="age" value="{{ old('age') }}"   autocomplete="age">
-
-                                    @error('age')
-                                        <div class="invalid-feedback col-12" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </div>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label for="height" class="col-form-label fs-6">{{ __('height') }}</label>
-
-                                <div class="col-12">
-                                    <input id="height" type="number" step="0.1" class="form-control text-center @error('height') is-invalid @enderror" name="height" value="{{ old('height') }}" autocomplete="height">
-
-                                    @error('height')
-                                        <div class="invalid-feedback col-12" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </div>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="d-flex justify-content-around mb-4">
-                            <div class="form-check form-check-inline is-invalid">
-                                <label for="male" class="form-check-label mr-2">{{ __('Male') }}</label>
-                                <input id="male" type="radio" class="form-check-input form-check-inline border border-dark @error('gender') is-invalid @enderror" name="gender" value="0" {{ old('gender') == '0' ? 'checked' : '' }}  autocomplete="gender">
-                            </div>
-                            <div class="form-check form-check-inline is-invalid">
-                                <label for="female" class="form-check-label mr-2">{{ __('Female') }}</label>
-                                <input id="female" type="radio" class="form-check-input form-check-inline border border-dark @error('gender') is-invalid @enderror" name="gender" value="1"  {{ old('gender') == '1' ? 'checked' : '' }}  autocomplete="gender">
-                            </div>
-                                @error('gender')
-                                    <div class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                @enderror
-                        </div>
-
-                        <div class="form-check form-check-inline d-flex mb-3">
-                            <select class="form-select @error('search_gender') is-invalid @enderror" aria-label="Default select example" name="search_gender" id="search_gender">
-                                <option value="">What you want to date?</option>
-                                @foreach (\App\Models\User::$search_genders as $key => $gender)
-                                    <option value="{{ $key }}" @if (old('search_gender') === $key) selected @endif>{{ $gender }}</option>
-                                @endforeach
-                            </select>
-                            @error('search_gender')
-                                <span class="invalid-feedback ml-3" role="alert">
+                              <div class="row">
+                                  <label for="height" class="col-form-label fs-6">{{ __('height') }}</label>
+                                  <div>
+                                      <input id="height" type="number" step="0.1" class="form-control text-center @error('height') is-invalid @enderror" name="height" value="{{ old('height') }}" autocomplete="height">
+                                  </div>
+                              </div>
+                          </div>
+                          <div>
+                            @error('age')
+                                <div class="invalid-feedback d-inline" role="alert">
                                     <strong>{{ $message }}</strong>
-                                </span>
+                                </div>
+                            @enderror
+                          </div>
+                          <div>
+                            @error('height')
+                                <div class="invalid-feedback d-inline" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </div>
+                            @enderror
+                          </div>
+                        </div>
+
+                        <div class="mb-4">
+                          <div class="d-flex justify-content-around">
+                              <div class="form-check form-check-inline is-invalid">
+                                  <label for="male" class="form-check-label mr-2">{{ __('Male') }}</label>
+                                  <input id="male" type="radio" class="form-check-input form-check-inline border border-dark @error('gender') is-invalid @enderror" name="gender" value="0" {{ old('gender') == '0' ? 'checked' : '' }}  autocomplete="gender">
+                              </div>
+                              <div class="form-check form-check-inline is-invalid">
+                                  <label for="female" class="form-check-label mr-2">{{ __('Female') }}</label>
+                                  <input id="female" type="radio" class="form-check-input form-check-inline border border-dark @error('gender') is-invalid @enderror" name="gender" value="1"  {{ old('gender') == '1' ? 'checked' : '' }}  autocomplete="gender">
+                              </div>
+                          </div>
+                            @error('gender')
+                              <div class="invalid-feedback d-inline" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </div>
                             @enderror
                         </div>
 
-                        <div class="form-check form-check-inline d-flex mb-3">
-                            <select class="form-select @error('search_status') is-invalid @enderror" aria-label="Default select example" name="search_status" id="search_status">
-                                <option value="">What you looking for?</option>
-                                @foreach (\App\Models\User::$search_statuses as $key => $status)
-                                    <option value="{{ $key }}" @if (old('search_status') === $key) selected @endif>{{ $status }}</option>
-                                @endforeach
-                            </select>
-                            @error('search_status')
-                                <span class="invalid-feedback ml-3" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                        <div class="mb-3">
+                          <div class="form-check form-check-inline d-flex">
+                              <select class="form-select @error('search_gender') is-invalid @enderror" aria-label="Default select example" name="search_gender" id="search_gender">
+                                  <option value="">Who you want to date?</option>
+                                  @foreach (\App\Models\User::$search_genders as $key => $gender)
+                                      <option value="{{ $key }}" @if (old('search_gender') === "$key") selected @endif>{{ $gender }}</option>
+                                  @endforeach
+                              </select>
+                            </div>
+                            <div>
+                              @error('search_gender')
+                                  <span class="invalid-feedback d-inline" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                          <div class="form-check form-check-inline d-flex">
+                              <select class="form-select @error('search_status') is-invalid @enderror" aria-label="Default select example" name="search_status" id="search_status">
+                                  <option value="">What do you want from your dates?</option>
+                                  @foreach (\App\Models\User::$search_statuses as $key => $status)
+                                      <option value="{{ $key }}" @if (old('search_status') === "$key") selected @endif>{{ $status }}</option>
+                                  @endforeach
+                              </select>
+                            </div>
+                            <div>
+                              @error('search_status')
+                                  <span class="invalid-feedback d-inline" role="alert">
+                                      <strong>{{ $message }}</strong>
+                                  </span>
+                              @enderror
+                            </div>
                         </div>
 
                         <div class="row mb-3">
