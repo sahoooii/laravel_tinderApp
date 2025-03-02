d8197867561a1f4d54df51dce7267fed1b84f313
sahoooii <saho.chu@gmail.com>
2025-03-02 09:51:54 +0900

    Remove phpinfo();

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SwipeController;
+use App\Http\Controllers\MatchController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::get('edit/{id}', [UserController::class, 'edit'])->name('users.edit');
+        Route::put('update/{id}', [UserController::class, 'update'])->name('users.update');
+        Route::post('destroy/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    });
+
+    Route::post('/swipes', [SwipeController::class, 'store'])->name('swipes.store');
+
+    Route::group(['prefix' => 'matches'], function () {
+        Route::get('', [MatchController::class, 'index'])->name('matches.index');
+        Route::get('show/{id}', [MatchController::class, 'show'])->name('matches.show');
+        Route::post('destroy/{id}', [MatchController::class, 'destroy'])->name('matches.destroy');
+    });
+});
